6bcdeee35e10897be0d88a66ec1da5813dc15d27
ShokirjonMK <mkshokirjon@gmail.com>
2025-06-02 08:55:07 +0500

    Update similar_qa.py

--- app/model/similar_qa.py
+++ app/model/similar_qa.py
@@ -1,10 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
 import json
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 BASE_DIR = os.path.dirname(__file__)
 qa_path = os.path.join(BASE_DIR, "qa_data.json")
+
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 
 
 def load_qa():
@@ -21,15 +22,15 @@ def get_best_answer(category, user_question):
     questions = [item['question'] for item in qa_pairs]
     answers = [item['answer'] for item in qa_pairs]
 
-    vectorizer = TfidfVectorizer().fit(questions + [user_question])
-    user_vec = vectorizer.transform([user_question])
-    question_vecs = vectorizer.transform(questions)
+    embeddings = model.encode(
+        questions + [user_question], convert_to_tensor=True)
+    user_emb = embeddings[-1]
+    sims = util.pytorch_cos_sim(user_emb, embeddings[:-1]).squeeze()
 
-    similarities = cosine_similarity(user_vec, question_vecs).flatten()
-    top_score = similarities.max()
-    top_index = similarities.argmax()
+    top_score = sims.max().item()
+    top_index = sims.argmax().item()
 
-    if top_score < 0.75:
-        return "Savol to‘liq mos kelmadi. Iltimos, savolni boshqacha shaklda yozib ko‘ring."
+    if top_score < 0.5:
+        return "Mazmunga mos savol topilmadi. Iltimos, aniqroq savol bering."
 
     return answers[top_index]
